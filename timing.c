ec9d16783255009b9f96d0397d05c06b0e49e7ef
Hikaru Nishida <hikalium@chromium.org>
2023-06-29 14:34:34 +0900

    update

--- timing.c
+++ timing.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 int main() {
   const char *pass = "3141";
-  char s[4];
+  char s[5];
   puts("Input your passcode (4 digits)");
-  fgets(s, 4, stdin);
+  fgets(s, sizeof(s), stdin);
   for (int i = 0; i < 4; i++) {
+    sleep(1);
     if (s[i] != pass[i]) {
       puts("Error!");
-      return 1;
+      return 0;
     }
   }
   puts("Welcome!");
